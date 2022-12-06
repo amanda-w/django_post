c48661e8f64ff3db4fdb5c4bf870b1fe01439969
amanda-w <amandanwong@gmail.com>
2022-12-05 21:41:44 -0800

    add: new views and tests

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -28,8 +29,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     # return HttpResponse("You're looking at question %s." % question_id)
@@ -43,6 +49,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
@@ -50,7 +61,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-    
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
